53bf479b823796855192ef1f8f0370748e396b3f
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2022-10-03 14:37:36 +0200

    Add: DigitalOcean provider for Crossplane
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

---
+++ hack/scripts/digitalocean.sh
@@ -0,0 +1,46 @@
+#! /usr/bin/env bash
+
+# Copyright (C) Nicolas Lamirault <nicolas.lamirault@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+reset_color="\\e[0m"
+color_red="\\e[31m"
+color_green="\\e[32m"
+color_blue="\\e[36m";
+
+# declare -r this_dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+# declare -r root_dir=$(cd ${this_dir}/../.. && pwd)
+
+function echo_fail { echo -e "${color_red}✖ $*${reset_color}"; }
+function echo_success { echo -e "${color_green}✔ $*${reset_color}"; }
+function echo_info { echo -e "${color_blue}$*${reset_color}"; }
+
+echo_info "[DigitalOcean] Configure DigitalOcean provider"
+
+[ -z "${DIGITALOCEAN_TOKEN}" ] && echo_fail "Environment variable DIGITALOCEAN_TOKEN not satisfied" && exit 1
+SECRET_NAME=$1
+NAMESPACE=$2
+
+DIGITALOCEAN_TOKEN_B64=$(echo "${DIGITALOCEAN_TOKEN}" | base64 | tr -d "\n")
+echo_info "[Kubernetes] DigitalOcean: Create secret ${SECRET_NAME} into ${NAMESPACE}"
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: ${SECRET_NAME}
+  namespace: ${NAMESPACE}
+type: Opaque
+data:
+  token: ${DIGITALOCEAN_TOKEN_B64}
+EOF
